fbc3441cff1bb6e6ba26a6cdb7a8ec4a9985aeb6
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-05-08 23:00:00 +0300

    locks refactored and implemented with colors

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,17 @@ const colorBoxes = document.getElementsByClassName("color-box");
 const generateBtn = document.getElementById("generate");
 const saveBtn = document.getElementById("save");
 const libraryBtn = document.getElementById("Library");
+const lockButtons = [...document.getElementsByClassName("lock-btn")];
+
+const toggleLockButton = (button) => {
+  if (button.children[0].style.display === "none") {
+    button.children[0].style.display = "block";
+    button.children[1].style.display = "none";
+  } else {
+    button.children[0].style.display = "none";
+    button.children[1].style.display = "block";
+  }
+};
 
 const loadLibrary = () => {
   const getLibrary = JSON.parse(localStorage.getItem("library"));
@@ -26,6 +37,12 @@ const updateColors = (colors) => {
     colorBoxes[i].style.backgroundColor = `${colors[i].value}`;
     colorBoxes[i].firstElementChild.textContent = colors[i].value;
   }
+};
+
+const checkLocks = (colors) => {
+  lockButtons.forEach((button, i) => {
+    colors[i].locked && toggleLockButton(button);
+  });
 };
 
 window.onload = () => {
@@ -58,10 +75,6 @@ const toggleLockColor = (colors, color) => {
   return colors;
 };
 
-for (let i = 0; i < colorBoxes.length; i++) {
-  colorBoxes[i].onclick = () => toggleLockColor(colors, i);
-}
-
 const generateColor = (colors) => {
   colors.forEach((color) => {
     if (!color.locked) {
@@ -74,56 +87,11 @@ const generateColor = (colors) => {
 
 generateBtn.onclick = () => generateColor(colors);
 
-const firstUnLockBtn = document.querySelector("#first-unlock-btn");
-const secondUnLockBtn = document.querySelector("#second-unlock-btn");
-const thirdUnLockBtn = document.querySelector("#third-unlock-btn");
-const fourthUnLockBtn = document.querySelector("#fourth-unlock-btn");
-const fifthUnLockBtn = document.querySelector("#fifth-unlock-btn");
+lockButtons.forEach((button, i) => {
+  toggleLockButton(button);
 
-const firstLockBtn = document.querySelector("#first-lock-btn");
-const secondLockBtn = document.querySelector("#second-lock-btn");
-const thirdLockBtn = document.querySelector("#third-lock-btn");
-const fourthLockBtn = document.querySelector("#fourth-lock-btn");
-const fifthLockBtn = document.querySelector("#fifth-lock-btn");
-
-firstUnLockBtn.addEventListener("click", () => {
-  firstUnLockBtn.style.display = "none";
-  firstLockBtn.style.display = "block";
-});
-secondUnLockBtn.addEventListener("click", () => {
-  secondUnLockBtn.style.display = "none";
-  secondLockBtn.style.display = "block";
-});
-thirdUnLockBtn.addEventListener("click", () => {
-  thirdUnLockBtn.style.display = "none";
-  thirdLockBtn.style.display = "block";
-});
-fourthUnLockBtn.addEventListener("click", () => {
-  fourthUnLockBtn.style.display = "none";
-  fourthLockBtn.style.display = "block";
-});
-fifthUnLockBtn.addEventListener("click", () => {
-  fifthUnLockBtn.style.display = "none";
-  fifthLockBtn.style.display = "block";
-});
-
-firstLockBtn.addEventListener("click", () => {
-  firstLockBtn.style.display = "none";
-  firstUnLockBtn.style.display = "block";
-});
-secondLockBtn.addEventListener("click", () => {
-  secondLockBtn.style.display = "none";
-  secondUnLockBtn.style.display = "block";
-});
-thirdLockBtn.addEventListener("click", () => {
-  thirdLockBtn.style.display = "none";
-  thirdUnLockBtn.style.display = "block";
-});
-fourthLockBtn.addEventListener("click", () => {
-  fourthLockBtn.style.display = "none";
-  fourthUnLockBtn.style.display = "block";
-});
-fifthLockBtn.addEventListener("click", () => {
-  fifthLockBtn.style.display = "none";
-  fifthUnLockBtn.style.display = "block";
+  button.onclick = () => {
+    toggleLockColor(colors, i);
+    toggleLockButton(button);
+  };
 });
