abd09bc67c78bd39fe689a1ef210a27271b6e39e
Omerrj <67536912+Omerrj@users.noreply.github.com>
2021-05-10 17:58:51 +0300

    library functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -17,17 +17,58 @@ const lockButtons = [...document.getElementsByClassName("lock-btn")];
 const hexColors = [...document.getElementsByClassName("hex-number")];
 const saveHexCode = document.getElementById("save-hexCode");
 
-const copied = () => {};
+const pallets = document.getElementById("pallets");
+const saveBtn = document.getElementById("save");
+const section = document.getElementById("main-container");
+const colorSave = document.getElementById("color-save");
+const libraryBtn = document.getElementById("Library");
+const colorLibrary = document.getElementById("color-library");
+const colorSaveClose = document.getElementById("color-save-close");
+const colorLibraryClose = document.getElementById("color-library-close");
+
+const togglePanel = (panel) => {
+  switch (panel) {
+    case "save":
+      {
+        if (section.style.display === "block") {
+          colorSave.style.display = "none";
+          section.style.display = "none";
+        } else {
+          section.style.display = "block";
+          colorSave.style.display = "block";
+        }
+      }
+
+      break;
+
+    case "library":
+      {
+        if (section.style.display === "block") {
+          colorLibrary.style.display = "none";
+          section.style.display = "none";
+        } else {
+          colorLibrary.style.display = "block";
+          section.style.display = "block";
+        }
+      }
+      break;
+  }
+
+  colorSaveInput.value = null;
+};
+
+const copied = () => {
+  section.style.display = "block";
+  saveHexCode.style.display = "block";
+  setTimeout(function () {
+    section.style.display = "none";
+    saveHexCode.style.display = "none";
+  }, 1500);
+};
 
 hexColors.forEach((hex) => {
   hex.onclick = () => {
     navigator.clipboard.writeText(hex.textContent).then(copied);
-    section.style.display = "block";
-    saveHexCode.style.display = "block";
-    setTimeout(function() {
-      section.style.display = "none";
-      saveHexCode.style.display = "none";
-     }, 1500);
   };
 });
 
@@ -41,9 +82,45 @@ const toggleLockButton = (button) => {
   }
 };
 
+const checkLocks = (colors) => {
+  lockButtons.forEach((button, i) => {
+    if (colors[i].locked) {
+      toggleLockButton(button);
+    }
+  });
+};
+
 const loadLibrary = () => {
   const getLibrary = JSON.parse(localStorage.getItem("library"));
   getLibrary && (library = getLibrary);
+
+  library.forEach((pallet) => {
+    const palletItem = document.createElement("div");
+    const palletColors = document.createElement("div");
+    const selectBtn = document.createElement("button");
+
+    selectBtn.onclick = () => {
+      colors = pallet.colors;
+      updateColors(colors);
+      togglePanel("library");
+      checkLocks(colors);
+    };
+
+    selectBtn.classList.add(`pallet-select-btn`);
+    selectBtn.textContent = "Select";
+
+    palletItem.classList.add("pallet-item");
+    palletItem.innerHTML = `<h2 class='pallet-name'>${pallet.name}</h2>`;
+
+    pallet.colors.forEach((color) => {
+      palletColors.innerHTML += `<div class='pallet-colors' style="background-color: ${color.value}"></div>`;
+    });
+
+    palletColors.append(selectBtn);
+    palletItem.append(palletColors);
+
+    pallets.append(palletItem);
+  });
 };
 
 const updateColors = (colors) => {
@@ -51,12 +128,6 @@ const updateColors = (colors) => {
     colorBoxes[i].style.backgroundColor = `${colors[i].value}`;
     colorBoxes[i].firstElementChild.textContent = colors[i].value;
   }
-};
-
-const checkLocks = (colors) => {
-  lockButtons.forEach((button, i) => {
-    colors[i].locked && toggleLockButton(button);
-  });
 };
 
 window.onload = () => {
@@ -93,69 +164,37 @@ const generateColor = (colors) => {
 
 generateColor(colors);
 
-generateBtn.onclick = () => {
-  generateColor(colors);
-};
-
 generateBtn.onclick = () => generateColor(colors);
 
 lockButtons.forEach((button, i) => {
   toggleLockButton(button);
 
   button.onclick = () => {
+    console.log(colors);
     toggleLockColor(colors, i);
     toggleLockButton(button);
   };
 });
 
-const saveBtn = document.getElementById("save");
-const section = document.getElementById("main-container");
-const colorSave = document.getElementById("color-save");
-const libraryBtn = document.getElementById("Library");
-const colorLibrary = document.getElementById("color-library");
-const colorSaveClose = document.getElementById("color-save-close");
-const colorLibraryClose = document.getElementById("color-library-close")
+saveBtn.onclick = () => togglePanel("save");
 
-saveBtn.onclick = () => {
-  section.style.display = "block";
-  colorSave.style.display = "block";
-}
-colorSaveClose.onclick = () =>{
-  section.style.display = "none";
-  colorSave.style.display = "none";
-}
-libraryBtn.onclick = () => {
-  colorLibrary.style.display = "block";
-  section.style.display = "block";
-}
+colorSaveClose.onclick = () => togglePanel("save");
+
+libraryBtn.onclick = () => togglePanel("library");
+
 colorLibraryClose.onclick = () => {
   colorLibrary.style.display = "none";
   section.style.display = "none";
-}
-const saveColorBtn = document.getElementById("save-color-btn");
-const colorSaveInput = document.getElementById("color-save-input");
-const closeColorSave = document.getElementById("close-color-save");
-
-const toggleSavePanel = () => {
-  if (section.style.display === "block") {
-    colorSave.style.display = "none";
-    section.style.display = "none";
-  } else {
-    section.style.display = "block";
-    colorSave.style.display = "block";
-  }
-
-  colorSaveInput.value = null;
 };
 
-saveBtn.onclick = () => toggleSavePanel();
+const saveColorBtn = document.getElementById("save-color-btn");
+const colorSaveInput = document.getElementById("color-save-input");
 
-closeColorSave.onclick = () => toggleSavePanel();
+saveBtn.onclick = () => togglePanel("save");
 
 saveColorBtn.onclick = () => {
   if (colorSaveInput.value) {
     save(colorSaveInput.value, colors);
-    toggleSavePanel();
+    togglePanel("save");
   } else alert("Please enter palette name");
 };
-
