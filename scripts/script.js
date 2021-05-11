52cf95173f5dda66b79170eccc4fd2279e938562
Ahmed183 <ahmed1831990@gmail.com>
2021-05-11 11:30:52 +0300

    Made the final review and reduced some codes

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,20 @@ function lightOrDark(color) {
         return 'dark';
     }
 
-}
+};
+
+const runColor = () => {
+
+    for (let i = 0; i < 5; i++) {
+        if (locked[i] == false) {
+            randomColors[i] = "#000000".replace(/0/g, function () {
+                return (~~(Math.random() * 16)).toString(16);
+            });
+        }
+    }
+    refreshColors();
+
+};
 
 const colors = document.querySelector("#colors");
 const libraryButton = document.querySelector("#libraryButton");
@@ -54,22 +67,8 @@ const refreshColors = () => {
         }
 
         const newLock = document.createElement("i");
-        if (i == 0) {
-            if (locked[0] == false) newLock.setAttribute("class", "fas fa-lock-open");
-            if (locked[0] == true) newLock.setAttribute("class", "fas fa-lock");
-        } else if (i == 1) {
-            if (locked[1] == false) newLock.setAttribute("class", "fas fa-lock-open");
-            if (locked[1] == true) newLock.setAttribute("class", "fas fa-lock");
-        } else if (i == 2) {
-            if (locked[2] == false) newLock.setAttribute("class", "fas fa-lock-open");
-            if (locked[2] == true) newLock.setAttribute("class", "fas fa-lock");
-        } else if (i == 3) {
-            if (locked[3] == false) newLock.setAttribute("class", "fas fa-lock-open");
-            if (locked[3] == true) newLock.setAttribute("class", "fas fa-lock");
-        } else if (i == 4) {
-            if (locked[4] == false) newLock.setAttribute("class", "fas fa-lock-open");
-            if (locked[4] == true) newLock.setAttribute("class", "fas fa-lock");
-        }
+        if (locked[i] == false) newLock.setAttribute("class", "fas fa-lock-open");
+        if (locked[i] == true) newLock.setAttribute("class", "fas fa-lock");
         newLock.setAttribute("id", color + "lock");
         newLock.style.width = "min-content";
         newLock.style.margin = "0px auto";
@@ -77,7 +76,7 @@ const refreshColors = () => {
 
         const newColor = document.createElement("div");
         newColor.setAttribute("class", "colorDiv");
-        newColor.style.backgroundColor = color.toUpperCase();
+        newColor.style.backgroundColor = color;
 
         newColor.append(colorTitle);
         newColor.append(newLock);
@@ -86,97 +85,31 @@ const refreshColors = () => {
     });
 
 }
-generateButton.addEventListener("click", () => {
-
-    for (let i = 0; i < 5; i++) {
-        if (locked[i] == false) {
-            randomColors[i] = "#000000".replace(/0/g, function () {
-                return (~~(Math.random() * 16)).toString(16);
-            });
-        }
-    }
-    refreshColors();
-
-});
-
-for (let i = 0; i < 5; i++) {
-    if (locked[i] == false) {
-        randomColors[i] = "#000000".replace(/0/g, function () {
-            return (~~(Math.random() * 16)).toString(16);
-        });
-    }
-}
-refreshColors();
+generateButton.addEventListener("click", runColor);
+runColor();
 
 colors.addEventListener("click", (e) => {
-    console.log(e.target);
     if (e.target.localName == "i") {
+        for (let i = 0; i < 5; i++) {
 
-        if (e.target.id == randomColors[0] + "lock") {
-            if (locked[0] == false) {
-                e.target.setAttribute("class", "fas fa-lock");
-                locked[0] = true;
-            } else if (locked[0] == true) {
-                e.target.setAttribute("class", "fas fa-lock-open");
-                locked[0] = false;
-            } else {
-                console.log("There is an error with lock1!");
+            if (e.target.id == randomColors[i] + "lock") {
+                if (locked[i] == false) {
+                    e.target.setAttribute("class", "fas fa-lock");
+                    locked[i] = true;
+                } else if (locked[i] == true) {
+                    e.target.setAttribute("class", "fas fa-lock-open");
+                    locked[i] = false;
+                } else {
+                    console.log(`There is an error with lock${i}!`);
+                }
             }
-        } else if (e.target.id == randomColors[1] + "lock") {
-            if (locked[1] == false) {
-                e.target.setAttribute("class", "fas fa-lock");
-                locked[1] = true;
-            } else if (locked[1] == true) {
-                e.target.setAttribute("class", "fas fa-lock-open");
-                locked[1] = false;
-            } else {
-                console.log("There is an error with lock2!");
-            }
-        } else if (e.target.id == randomColors[2] + "lock") {
-            if (locked[2] == false) {
-                e.target.setAttribute("class", "fas fa-lock");
-                locked[2] = true;
-            } else if (locked[2] == true) {
-                e.target.setAttribute("class", "fas fa-lock-open");
-                locked[2] = false;
-            } else {
-                console.log("There is an error with lock3!");
-            }
-        } else if (e.target.id == randomColors[3] + "lock") {
-            if (locked[3] == false) {
-                e.target.setAttribute("class", "fas fa-lock");
-                locked[3] = true;
-            } else if (locked[3] == true) {
-                e.target.setAttribute("class", "fas fa-lock-open");
-                locked[3] = false;
-            } else {
-                console.log("There is an error with lock4!");
-            }
-        } else if (e.target.id == randomColors[4] + "lock") {
-            if (locked[4] == false) {
-                e.target.setAttribute("class", "fas fa-lock");
-                locked[4] = true;
-            } else if (locked[4] == true) {
-                e.target.setAttribute("class", "fas fa-lock-open");
-                locked[4] = false;
-            } else {
-                console.log("There is an error with lock5!");
-            }
-        } else {
-            console.log("There is an error with the lock elements!");
+
         }
-
     }
 
     if (e.target.id.length == 7) {
 
-        var text = document.getElementById(e.target.id).innerText;
-        var elem = document.createElement("textarea");
-        document.body.appendChild(elem);
-        elem.value = text;
-        elem.select();
-        document.execCommand("copy");
-        document.body.removeChild(elem);
+        window.navigator.clipboard.writeText(e.target.id.toUpperCase());
         copyModal.style.display = "block";
         setTimeout(() => {
             copyModal.style.display = "none";
